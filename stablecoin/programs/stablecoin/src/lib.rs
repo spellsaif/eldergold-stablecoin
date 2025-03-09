f1fabdd748e22d1806a1c7306e71fa2e60b21db8
Saif <71248588+spellsaif@users.noreply.github.com>
2025-03-09 23:43:15 +0000

    initialized anchor stablecoin project

---
+++ stablecoin/programs/stablecoin/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Co32qABaBxtzVNmSmQ82v5MXzcCvRvUb23FmfW6Ko7JR");
+
+#[program]
+pub mod stablecoin {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
