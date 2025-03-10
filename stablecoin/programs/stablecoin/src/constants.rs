767b1409db3a463bae43abfce23ec124bce99f68
Saif <71248588+spellsaif@users.noreply.github.com>
2025-03-10 19:24:42 +0000

    fixes: added missing types for some constants

--- stablecoin/programs/stablecoin/src/constants.rs
+++ stablecoin/programs/stablecoin/src/constants.rs
@@ -3,4 +3,4 @@ use anchor_lang::prelude::*;
 pub const SEED_CONFIG_ACCOUNT: &[u8] = b"config";
 pub const SEED_MINT_ACCOUNT: &[u8] = b"mint";
 
-pub const MINT_DECIMALS = 9;
+pub const MINT_DECIMALS:u64 = 9;
