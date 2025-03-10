676f3c9d1580de7d9d989ae88a87d1d3bd6900d6
Saif <71248588+spellsaif@users.noreply.github.com>
2025-03-10 17:52:55 +0000

    added: config state

--- stablecoin/programs/stablecoin/src/states/config.rs
+++ stablecoin/programs/stablecoin/src/states/config.rs
@@ -5,5 +5,10 @@ use anchor_lang::prelude::*;
 pub struct Config {
     pub authority: Pubkey,
     pub mint_account: Pubkey,
+    pub liquidation_threshold:u64,
+    pub liquidation_bonus:u64,
+    pub min_health_factor: u64,
+    pub bump: u8,
+    pub bump_mint_account: u8,
     
 }
