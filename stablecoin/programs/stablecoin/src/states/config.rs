761a39e3d891a05625ba7f37b61fe70f4d3732c4
Saif <71248588+spellsaif@users.noreply.github.com>
2025-03-10 19:23:06 +0000

    rename: Config to ConfigState

--- stablecoin/programs/stablecoin/src/states/config.rs
+++ stablecoin/programs/stablecoin/src/states/config.rs
@@ -2,13 +2,13 @@ use anchor_lang::prelude::*;
 
 #[account]
 #[derive(InitSpace)]
-pub struct Config {
+pub struct ConfigState {
     pub authority: Pubkey,
     pub mint_account: Pubkey,
     pub liquidation_threshold:u64,
     pub liquidation_bonus:u64,
     pub min_health_factor: u64,
     pub bump: u8,
-    pub bump_mint_account: u8,
+    pub bump_mint_account:u8
     
 }
